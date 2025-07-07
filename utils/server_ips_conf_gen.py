1464b27172972e5f9eea736dcca58c7a94092607
Mxd353 <13154213167@163.com>
2025-07-07 13:05:54 +0800

    生成server_ips.conf的IP地址生成器

---
+++ utils/server_ips_conf_gen.py
@@ -0,0 +1,20 @@
+def generate_ips():
+    lines = []
+    for i in range(7, 39):
+        lines.append("# Rack {}".format(i - 7))
+        lines.append("ens13f0 192.168.56.103")
+        for j in range(1, 33):
+            lines.append("192.168.{}.{}".format(i, j))
+        lines.append("")
+    return lines
+
+def main():
+    ips = generate_ips()
+    with open("server_ips.conf", "w") as f:
+        for ip in ips:
+            f.write(ip + "\n")
+    print("Generated server_ips.conf with {} addresses.".format(len(ips)))
+
+
+if __name__ == "__main__":
+    main()
