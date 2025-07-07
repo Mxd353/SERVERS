158f8b024b6c8f5e6099af4aadee242828ab35d6
Mxd353 <13154213167@163.com>
2025-07-07 13:07:43 +0800

    移除run.sh中的LD_LIBRARY_PATH输出日志；新增server_ips.conf配置文件，添加多个机架的IP地址

--- run.sh
+++ run.sh
@@ -17,8 +17,8 @@ fi
 
 export LD_LIBRARY_PATH="$GCC_HOME/lib64:$BOOST_HOME/lib:$RTE_SDK/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
 
-echo "[INFO] 使用的 LD_LIBRARY_PATH:"
-echo "       $LD_LIBRARY_PATH"
+# echo "[INFO] 使用的 LD_LIBRARY_PATH:"
+# echo "       $LD_LIBRARY_PATH"
 
 if [[ ! -f "./build/run" ]]; then
     echo "[ERROR] 找不到可执行文件 ./build/run"
